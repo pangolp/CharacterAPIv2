dc4be41b6dc52283ef37ad9c30f081838011cc6b
Pagani Walter <paganiwalter@gmail.com>
2020-08-26 10:36:05 -0300

    Agregando routers, para generar las url para Rating.
    Se agregan las importaciones de Rest Framework y se construye la URL raíz de la api.

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
 from .views import (
 	getCharacterById,
+	RatingViewSet,
 )
 
+router = routers.DefaultRouter()
+router.register('ratings', RatingViewSet)
+
 urlpatterns = [
+	path('', include(router.urls)),
     path('character/<int:id>/', getCharacterById),
 ]
