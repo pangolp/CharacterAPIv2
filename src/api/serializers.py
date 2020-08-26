4889b6f13bcc6eeeaf1164386a1b603aaf3e6ebc
Pagani Walter <paganiwalter@gmail.com>
2020-08-26 15:24:50 -0300

    Mejorando los serializers
    Se cambió HyperlinkedModelSerializer por ModelSerializer. Se crearon los serializers de los modelos faltantes. También se organizó mejor el código para que sea legible.

--- src/api/serializers.py
+++ src/api/serializers.py
@@ -1,8 +1,43 @@
 from rest_framework import serializers
-from .models import Rating
+from .models import Planet, Character, Rating
 
 
-class RatingSerializer(serializers.HyperlinkedModelSerializer):
+class PlanetSerializer(serializers.ModelSerializer):
+
 	class Meta:
+		fields = (
+			'name',
+			'population',
+			'known_residents_count'
+		)
+		model = Planet
+
+
+class CharacterSerializer(serializers.ModelSerializer):
+
+	class Meta:
+		fields = (
+			'name',
+			'height',
+			'mass',
+			'hair_color',
+			'skin_color',
+			'eye_color',
+			'birth_year',
+			'gender',
+			'homeworld',
+			'species_name',
+			'average_rating',
+			'max_rating'
+		)
+		model = Character
+
+
+class RatingSerializer(serializers.ModelSerializer):
+
+	class Meta:
+		fields = (
+			'character_id',
+			'score'
+		)
 		model = Rating
-		fields = ['character_id', 'score']
