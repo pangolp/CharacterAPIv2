5dd0c22184dfb6ecc97bc0dced1a7d1e011d7888
Pagani Walter <paganiwalter@gmail.com>
2020-08-26 14:28:01 -0300

    Error en el validador, el valor debía ser solo mayor que 83.

--- src/api/validators.py
+++ src/api/validators.py
@@ -7,5 +7,5 @@ def score_validation(value):
 
 
 def character_id_validation(value):
-	if value <= 0 or value >=83:
+	if value <= 0 or value > 83:
 		raise ValidationError('El character id, debe ser un número mayor que 0 y menor que 84.')
