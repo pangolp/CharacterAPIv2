109bc4629d214b9a3cf91d892de1ede8423c12db
Pagani Walter <paganiwalter@gmail.com>
2020-08-26 14:23:33 -0300

    No existen character mayores a 83.
    Limitando el id de character, debido a que en la documentación de la api, dice que el mayor id es el número 83.

--- src/api/validators.py
+++ src/api/validators.py
@@ -7,5 +7,5 @@ def score_validation(value):
 
 
 def character_id_validation(value):
-	if value <= 0:
-		raise ValidationError('El id del character debe ser un número mayor que cero.')
+	if value <= 0 or value >=83:
+		raise ValidationError('El character id, debe ser un número mayor que 0 y menor que 84.')
