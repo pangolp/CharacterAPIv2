ce874e6b390cd18740b72a6b46d457424bdc09d0
Pagani Walter <paganiwalter@gmail.com>
2020-08-26 09:22:15 -0300

    Primera view del api
    Pude construir una pequeña vista, que logra retorna la mayoría de los valores de la api que estamos consultando, pero a pesar de tener una librería para python, que la instale, no pude hacerla funcionar bajo ningún punto de vista, por eso no me quedo otra opción que armarlo provisoriamente de este modo.

--- src/api/views.py
+++ src/api/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+import requests
+from django.http import JsonResponse
+from .models import Character
 
-# Create your views here.
+
+def getCharacterById(request, id):
+	character = Character()
+	link = "https://swapi.dev/api/people/" + str(id) + "/"
+	response = requests.get(link)
+	response = response.json()
+	character.name = response['name']
+	character.height = response['height']
+	character.mass = response['mass']
+
+	urlPlanet = response['homeworld']
+	responsePlanet = requests.get(urlPlanet)
+	responsePlanet = responsePlanet.json()
+	residents = 0
+	for i in responsePlanet['residents']:
+		residents = residents + 1
+
+	response = JsonResponse(
+		{
+			'name': character.name,
+			'height': character.height,
+			'mass': character.mass,
+			'hair_color': response['hair_color'],
+			'skin_color': response['skin_color'],
+			'eye_color': response['eye_color'],
+			'birth_year': response['birth_year'],
+			'gender': response['gender'],
+			'homeworld': {
+				'name': responsePlanet['name'],
+				'population': responsePlanet['population'],
+				'known_residents_count': residents,
+			},
+			'species_name': '',
+			'average_rating': 4.5,
+			'max_rating': 5,
+		}
+	)
+	return response
